f4a0ea166d872977289b22948f7615ed7f11156d
Rodolph Van Romondt <mr.rodolph@gmail.com>
2024-01-06 05:54:58 -0800

    create route to update/delete Cupcake

--- app.py
+++ app.py
@@ -42,3 +42,29 @@ def create_cupcake():
     db.session.commit()
 
     return jsonify(cupcake=cupcake.to_json()), 201
+
+@app.route("/api/cupcakes/<int:id>", methods=["PATCH"])
+def update_cupcake(id):
+    """ Update/PATCH a cupcake and return it as JSON. """
+
+    cupcake = Cupcake.query.get_or_404(id)
+
+    cupcake.flavor = request.json.get("flavor", cupcake.flavor)
+    cupcake.size = request.json.get("size", cupcake.size)
+    cupcake.rating = request.json.get("rating", cupcake.rating)
+    cupcake.image = request.json.get("image", cupcake.image)
+
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.to_json())
+
+@app.route("/api/cupcakes/<int:id>", methods=["DELETE"])
+def delete_cupcake(id):
+    """ DELETE a cupcake and return it as JSON. """
+
+    cupcake = Cupcake.query.get_or_404(id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Deleted")
